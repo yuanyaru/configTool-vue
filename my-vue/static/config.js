6b73198d2611c85c3e8c957f0f70cb95271a362e
yuanyaru <398916756@qq.com>
2022-03-28 08:36:10 +0800

    update

--- my-vue/static/config.js
+++ my-vue/static/config.js
@@ -1,4 +1,4 @@
 var BASE_INFO = {
-    back_URL:'http://127.0.0.1:5000',  //python服务端
+    back_URL:'http://192.168.100.170:5005',  //python服务端
     monitoring_URL: 'http://192.168.100.170:8088',  //监控界面
 }
